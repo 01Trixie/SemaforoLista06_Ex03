843c737da9386d15030f544dd442def4cff7c5a1
Beatriz Oliveira <161655658+01Trixie@users.noreply.github.com>
2024-03-28 11:31:41 -0300

    Update Circuito.java

--- src/view/Circuito.java
+++ src/view/Circuito.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.util.Arrays;
 import java.util.concurrent.Semaphore;
 
 import controller.CircuitoController;
@@ -7,13 +8,20 @@ import controller.CircuitoController;
 public class Circuito {
 
 	public static void main(String[] args) {
-		
+		CircuitoController vetor [] = new CircuitoController[25];
 		int permissao = 5;
 		Semaphore semaforo = new Semaphore(permissao);
 		
 		for(int idAtleta = 0; idAtleta < 25; idAtleta++) {
-			Thread tCorre = new CircuitoController(idAtleta, semaforo);
-			tCorre.start();
+			vetor[idAtleta] = new CircuitoController(idAtleta, semaforo);
+			vetor[idAtleta].start();
+		}
+		while(CircuitoController.posChegada < 25) {
+			System.out.print("");
+		}
+		Arrays.sort(vetor);
+		for(int i = 0; i < 25; i++) {
+			System.out.println( "O atleta # " + vetor[i].idAtleta + " fez " + vetor[i].pontos);
 		}
 	}
 
